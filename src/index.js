3621f14d7b0d306dd7dbdfffedf4b6e0d3468f08
csandu <sandu.cristian@mail.ru>
2019-09-25 19:46:01 +0300

    IE 11 > Styles fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import React from 'react'
 import { render } from 'react-dom'
-
 import App from './App'
+
 import { register as registerServiceWorker } from './utils/serviceWorker'
 
 import './styles/index.css'
